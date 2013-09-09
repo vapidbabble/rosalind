4f6e990d9dccab93507fa007b6803cd494ebcd55
ralmlopez <ralmlopez@hotmail.com>
2013-09-09 12:16:16 -0500

    My F# solution

---
+++ ralmlopez-fsharp/countdna.fsx
@@ -0,0 +1,16 @@
+let data = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGT GGATTAAAAAAAGAGTGTCTGATAGCAGC"
+let list = Array.toList (data.ToCharArray())
+
+let count_dna list =
+    let rec count list (A,C,G,T) =
+        match list with 
+        | 'A'::xs -> count xs (A+1,C,G,T)
+        | 'C'::xs -> count xs (A,C+1,G,T)
+        | 'G'::xs -> count xs (A,C,G+1,T)
+        | 'T'::xs -> count xs (A,C,G,T+1)
+        | _::xs -> count xs (A,C,G,T)
+        | [] -> (A,C,G,T)
+    count list (0,0,0,0)
+
+let (A,C,G,T) = count_dna list
+printfn "%i %i %i %i" A C G T 
